8470fb3671cc226765c87ccbcac2f9d179c46ff4
GueziLouay <adelnn8@gmail.com>
2023-03-06 16:29:07 +0100

    downgrade to la99

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    
+    public function AdminDashboard(){
+
+        return view('admin.index');
+        
+    }// End method
+
+    public function AdminLogin(){
+
+        return view('admin.admin_login');
+
+    }
+
+    public function AdminDestory(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }
+}
